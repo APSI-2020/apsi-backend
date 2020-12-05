1d46541c550d80e0c5ebfad9188a4d84e03a48d3
Piotr Proszowski <p.proszowski@gmail.com>
2020-12-05 19:03:09 +0100

    Introduce academic titles enum

--- users/models.py
+++ users/models.py
@@ -8,8 +8,21 @@ class UserType(Enum):
     GUEST = "GUEST"
 
 
+class AcademicTitle(Enum):
+    lic = "lic."
+    inz = "inż."
+    mgr = "mgr."
+    dr = "dr."
+    prof = "prof."
+    prof_inz = "prof. inż."
+    mgr_inz = "mgr inż."
+    dr_inz = "dr inż."
+    dr_hab = "dr hab."
+    dr_hab_inz = "dr hab. inż."
+
+
 class UsersTypes(models.Model):
-    name = models.CharField(max_length=8,
+    name = models.CharField(max_length=32,
                             choices=[(tag, tag.value) for tag in UserType])
 
 
@@ -21,7 +34,8 @@ class User(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.CharField(max_length=128)
-    title = models.CharField(max_length=64, null=True)
+    title = models.CharField(max_length=32,
+                             choices=[(tag, tag.value) for tag in AcademicTitle], null=True)
     password_hash = models.CharField(max_length=64)
     groups = models.ManyToManyField(UsersGroups)
     types = models.ManyToManyField(UsersTypes)
