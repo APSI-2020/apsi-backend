ccc5d867227d9df85d2770ddeeefb2bd944d98b0
Piotr Kramek <peterkramek@gmail.com>
2020-12-05 19:32:35 +0100

    Update models

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from requirements.models import Requirements
+from users.models import Users
 
 
 class Places(models.Model):
@@ -14,7 +15,10 @@ class Events(models.Model):
     limitOfParticipants = models.IntegerField()
     price = models.DecimalField(null=True, decimal_places=2, max_digits=6)
     place = models.ForeignKey(Places, on_delete=models.CASCADE)
+
     requirements = models.OneToOneField(Requirements, on_delete=models.CASCADE, null=True)
+    lecturers = models.ManyToManyField(Users, related_name='hosted_events')
+    participants = models.ManyToManyField(Users, related_name='events')
 
 
 class Tickets(models.Model):
--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class UsersGroups(models.Model):
     name = models.CharField(max_length=64)
 
 
-class User(models.Model):
+class Users(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.CharField(max_length=128)
