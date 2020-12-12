eddb556654d71fde86e18c1b3c12d5e6f2b29b7a
Piotr Kramek <peterkramek@gmail.com>
2020-12-12 16:54:34 +0100

    Add filtering of events

--- events/events_repository.py
+++ events/events_repository.py
@@ -1,12 +1,17 @@
+from datetime import datetime
+
+from dateutil.parser import parse
+from django.db.models import Q
+
 from events.models import Events
-import time
 
 
 class EventsRepository:
     def __init__(self):
         pass
 
-    def find_events_for_given_with_respect_to_filters(self, filters):
+    def find_events_for_given_with_respect_to_filters(self, request):
+        filters = dict(request.GET)
         query = Events.objects
         date_from = filters.get('date_from', None)
         date_to = filters.get('date_to', None)
@@ -17,18 +22,36 @@ class EventsRepository:
         place = filters.get('place', None)
 
         if date_from:
-            query = query.filter(start__gte=date_from)
+            datetime_start = parse(date_from[0])
+            date_filter = Q(start__date__gte=datetime_start)
+            time_filter = Q(start__time__gte=datetime_start)
+            query = query.filter(date_filter & time_filter)
+
         if date_to:
-            query = query.filter(end__lte=date_to)
+            datetime_end = parse(date_to[0])
+            date_filter = Q(start__date__lte=datetime_end)
+            time_filter = Q(start__time__lte=datetime_end)
+            query = query.filter(date_filter & time_filter)
+
         if name_contains:
-            query = query.filter(name__icontains=name_contains)
+            query = query.filter(name__icontains=name_contains[0])
+
         if tags:
-            query = query.filter(tags__in=tags)
-        if past_events:
-            query = query.filter(end__lte=time.localtime())
-        if price:
-            query = query.filter(price__lte=price)
+            query = query.filter(tags__in=tags[0])
+
+        if past_events is not None:
+            if past_events is True:
+                now = datetime.now()
+                date_filter = Q(start__date__lte=now)
+                time_filter = Q(start__time__lte=now)
+                query = query.filter(date_filter & time_filter)
+
+        if price is not None:
+            query = query.filter(price__lte=float(price[0]))
+        else:
+            query = query.filter(price__isnull=True)
+
         if place:
-            query = query.filter(place=place)
+            query = query.filter(place__name__icontains=place[0])
 
         return query.all()
--- events/views.py
+++ events/views.py
@@ -1,9 +1,9 @@
-from django.http import JsonResponse, HttpResponseNotFound, HttpResponse
+from django.http import JsonResponse
 
+from events.events_repository import EventsRepository
 from events.serializers import EventSerializer
 from requirements.requirements_checker import RequirementsChecker
 from users.users_service import UsersService
-from events.events_repository import EventsRepository
 
 
 def does_user_meet_requirements(event, user):
@@ -15,7 +15,7 @@ def get_events(request):
     users_service = UsersService()
     user = users_service.fetch_by_jwt(jwt)
     events_repository = EventsRepository()
-    events = events_repository.find_events_for_given_with_respect_to_filters(dict(price=12.12))
+    events = events_repository.find_events_for_given_with_respect_to_filters(request)
     available_events = list(filter(lambda event: does_user_meet_requirements(event, user), events))
     serializer = EventSerializer(available_events, context=dict(user=user), many=True)
     response = serializer.data
