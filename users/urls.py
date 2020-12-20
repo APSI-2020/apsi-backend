a9d5c0e4a19ba6376006c338cf5dc2c9d0386a33
Michal Zebrowski <michal.zebrowski@valueadd.pl>
2020-12-20 16:31:42 +0100

    feat(): add current user view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import UserCreate, HelloWorldView, UserGroupsView
+from .views import UserCreate, CurrentUserView, UserGroupsView
 
 urlpatterns = [
-    path('user/hello/', HelloWorldView.as_view(), name='hello_world'),
+    path('user/current/', CurrentUserView.as_view(), name='current_user'),
     path('user/user-groups/', UserGroupsView.as_view(), name='get_user_groups'),
     path('auth/register/', UserCreate.as_view(), name="create_user"),
     path('auth/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
