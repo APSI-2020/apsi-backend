e2fdecfb5b793ab6127cd43148abdd98e6609cc5
PKramek <37769346+PKramek@users.noreply.github.com>
2020-12-20 17:20:53 +0100

    Feature/lecturers list (#17)
    * Add admin panel and register models
    
    * Split admin.py into apps
    
    * Fix User title
    
    * Add databese migration
    
    * Add lecturers endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import UserCreate, CurrentUserView, UserGroupsView
+from .views import UserCreate, CurrentUserView, UserGroupsView, LecturersView
+
 
 urlpatterns = [
     path('user/current/', CurrentUserView.as_view(), name='current_user'),
     path('user/user-groups/', UserGroupsView.as_view(), name='get_user_groups'),
+    path('user/lecturers/', LecturersView.as_view(), name='get_lecturers'),
     path('auth/register/', UserCreate.as_view(), name="create_user"),
     path('auth/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
--- users/views.py
+++ users/views.py
@@ -7,7 +7,8 @@ from rest_framework.authentication import get_authorization_header
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSerializer, UserGroupSerializer
+from .serializers import UserSerializer, UserGroupSerializer, LecturerSerializer
+from .users_repository import LecturerRepository
 from .users_repository import UsersGroupRepository
 from .users_service import UsersService
 
@@ -59,3 +60,32 @@ class UserGroupsView(APIView):
             return JsonResponse(response, safe=False)
         else:
             return JsonResponse(data=[], status=status.HTTP_404_NOT_FOUND, safe=False)
+
+
+class LecturersView(APIView):
+    lecturers_repository = LecturerRepository()
+    users_service = UsersService()
+
+    lecturers_response = openapi.Response('Return a list of all lecturers', LecturerSerializer(many=True))
+    authorization_token = openapi.Parameter('Authorization', openapi.IN_HEADER,
+                                            description="Authorization token which starts with Bearer",
+                                            type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(operation_description='Endpoint for retrieving all lecturers.',
+                         responses={200: lecturers_response, 404: [], 403: []},
+                         manual_parameters=[authorization_token])
+    def get(self, request):
+        jwt = request.headers['Authorization']
+        user = self.users_service.fetch_by_jwt(jwt)
+
+        if not user:
+            return JsonResponse(data=None, status=status.HTTP_403_FORBIDDEN, safe=False)
+
+        lecturers = self.lecturers_repository.get_all_lecturers()
+
+        if not lecturers:
+            return JsonResponse(data=[], status=status.HTTP_404_NOT_FOUND, safe=False)
+
+        serializer = LecturerSerializer(lecturers, many=True)
+        response = serializer.data
+        return JsonResponse(response, safe=False)
