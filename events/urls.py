f00b6aeb959dd1ae0b59a62a7b7504ebb1c8baf1
Piotr Proszowski <p.proszowski@gmail.com>
2020-12-20 14:43:58 +0100

    Join to event (#13)
    * Join to event
    
    * feat(): change return status http code

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('places/', Places.as_view(), name="places"),
     path('events/', Events.as_view(), name="events"),
-    path('events/<int:event_id>/', Event.as_view(), name='event')
+    path('events/<int:event_id>/', Event.as_view(), name='event'),
+    path('events/<int:event_id>/join', JoinEvent.as_view(), name='join_event')
 ]
